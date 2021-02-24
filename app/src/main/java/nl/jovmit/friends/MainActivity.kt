a406866be435c96f3cb01e13cbaff2e0346569c7
Jov Mit <mitrejcevski@gmail.com>
2021-02-25 00:17:10 +0100

    Update to compose beta01

--- app/src/main/java/nl/jovmit/friends/MainActivity.kt
+++ app/src/main/java/nl/jovmit/friends/MainActivity.kt
@@ -1,12 +1,12 @@
 package nl.jovmit.friends
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import nl.jovmit.friends.ui.theme.FriendsTheme
 
