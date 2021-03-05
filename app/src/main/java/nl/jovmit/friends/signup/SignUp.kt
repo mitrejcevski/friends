67f5e77f4be35c36639080a9f4738b129e01ceb2
Jov Mit <mitrejcevski@gmail.com>
2021-03-05 02:27:56 +0100

    Improved the look and feel of the sign up screen

--- app/src/main/java/nl/jovmit/friends/signup/SignUp.kt
+++ app/src/main/java/nl/jovmit/friends/signup/SignUp.kt
@@ -1,7 +1,6 @@
 package nl.jovmit.friends.signup
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -10,14 +9,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import nl.jovmit.friends.R
+import nl.jovmit.friends.ui.theme.typography
 
 @Composable
 @Preview(device = Devices.PIXEL_4)
 fun SignUp() {
-  Column(modifier = Modifier.fillMaxSize()) {
-    Text(text = "Create an account")
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(16.dp)
+  ) {
+    Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.Center
+    ) {
+      Text(
+        text = stringResource(R.string.createAnAccount),
+        style = typography.h4
+      )
+    }
+    Spacer(modifier = Modifier.height(16.dp))
     OutlinedTextField(
+      modifier = Modifier.fillMaxWidth(),
       value = "",
       label = {
         Text(text = stringResource(id = R.string.email))
@@ -25,13 +40,16 @@ fun SignUp() {
       onValueChange = { }
     )
     OutlinedTextField(
+      modifier = Modifier.fillMaxWidth(),
       value = "",
       label = {
         Text(text = stringResource(id = R.string.password))
       },
       onValueChange = { }
     )
+    Spacer(modifier = Modifier.height(8.dp))
     Button(
+      modifier = Modifier.fillMaxWidth(),
       onClick = { }
     ) {
       Text(text = stringResource(id = R.string.signUp))
