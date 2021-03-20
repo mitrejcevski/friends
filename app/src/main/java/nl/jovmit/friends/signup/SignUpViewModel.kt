7240e2f7ecb970b47f2f38852d1d958ebd858dc9
Jov Mit <mitrejcevski@gmail.com>
2021-03-20 02:08:38 +0100

    Introduced credentials validation results

--- app/src/main/java/nl/jovmit/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/nl/jovmit/friends/signup/SignUpViewModel.kt
@@ -15,16 +15,36 @@ class SignUpViewModel {
     password: String,
     about: String
   ) {
-    val emailRegex = """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+    when (validate(email, password)) {
+      is CredentialsValidationResult.InvalidEmail ->
+        _mutableSignUpState.value = SignUpState.BadEmail
+      is CredentialsValidationResult.InvalidPassword ->
+        _mutableSignUpState.value = SignUpState.BadPassword
+    }
+  }
+
+  private fun validate(
+    email: String,
+    password: String
+  ): CredentialsValidationResult {
+    val emailRegex =
+      """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
     val emailPattern = Pattern.compile(emailRegex)
     val passwordRegex = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
     val passwordPattern = Pattern.compile(passwordRegex)
 
-    val state = if(!emailPattern.matcher(email).matches()) {
-       SignUpState.BadEmail
-    } else if(!passwordPattern.matcher(password).matches()) {
-       SignUpState.BadPassword
+    val result = if (!emailPattern.matcher(email).matches()) {
+      CredentialsValidationResult.InvalidEmail
+    } else if (!passwordPattern.matcher(password).matches()) {
+      CredentialsValidationResult.InvalidPassword
     } else TODO()
-    _mutableSignUpState.value = state
+    return result
+  }
+
+  sealed class CredentialsValidationResult {
+
+    object InvalidEmail : CredentialsValidationResult()
+
+    object InvalidPassword : CredentialsValidationResult()
   }
 }
