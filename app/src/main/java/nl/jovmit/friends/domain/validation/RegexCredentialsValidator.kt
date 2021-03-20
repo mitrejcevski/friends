c194424288853537b3cd8f752170ad2cdea73c81
Jov Mit <mitrejcevski@gmail.com>
2021-03-20 02:19:50 +0100

    Removed duplication

--- app/src/main/java/nl/jovmit/friends/domain/validation/RegexCredentialsValidator.kt
+++ app/src/main/java/nl/jovmit/friends/domain/validation/RegexCredentialsValidator.kt
@@ -3,23 +3,28 @@ package nl.jovmit.friends.domain.validation
 import java.util.regex.Pattern
 
 class RegexCredentialsValidator {
+
+  private companion object {
+    private const val EMAIL_REGEX =
+      """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+
+    private const val PASSWORD_REGEX =
+      """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
+  }
+
+  private val emailPattern = Pattern.compile(EMAIL_REGEX)
+  private val passwordPattern = Pattern.compile(PASSWORD_REGEX)
+
   fun validate(
     email: String,
     password: String
   ): CredentialsValidationResult {
-    val emailRegex =
-      """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-    val emailPattern = Pattern.compile(emailRegex)
-    val passwordRegex = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
-    val passwordPattern = Pattern.compile(passwordRegex)
-
-    val result = if (!emailPattern.matcher(email).matches()) {
+    return if (!emailPattern.matcher(email).matches()) {
       CredentialsValidationResult.InvalidEmail
     } else if (!passwordPattern.matcher(password).matches()) {
       CredentialsValidationResult.InvalidPassword
     } else {
       CredentialsValidationResult.Valid
     }
-    return result
   }
 }
