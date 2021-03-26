70efb4cd8a4460271456d390dc58d7c454a9c238
Jov Mit <mitrejcevski@gmail.com>
2021-03-27 00:15:12 +0100

    Emerged a class to manage users

--- app/src/main/java/nl/jovmit/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/nl/jovmit/friends/signup/SignUpViewModel.kt
@@ -29,16 +29,49 @@ class SignUpViewModel(
     }
   }
 
+  val userCatalog = InMemoryUserCatalog()
+
   private fun signUp(
     email: String,
     password: String,
     about: String
   ): SignUpState {
     return try {
-      val user = createUser(email, password, about)
+      val user = userCatalog.createUser(email, password, about)
       SignUpState.SignedUp(user)
     } catch (duplicateAccount: DuplicateAccountException) {
       SignUpState.DuplicateAccount
+    }
+  }
+
+  class InMemoryUserCatalog(
+    private val usersForPassword: MutableMap<String, MutableList<User>> = mutableMapOf()
+  ) {
+
+    fun createUser(
+      email: String,
+      password: String,
+      about: String
+    ): User {
+      checkAccountExists(email)
+      val userId = createUserIdFor(email)
+      val user = User(userId, email, about)
+      saveUser(password, user)
+      return user
+    }
+
+    private fun saveUser(password: String, user: User) {
+      usersForPassword.getOrPut(password, ::mutableListOf).add(user)
+    }
+
+    private fun createUserIdFor(email: String): String {
+      return email.takeWhile { it != '@' } + "Id"
+    }
+
+    private fun checkAccountExists(email: String) {
+      if (usersForPassword.values.flatten().any { it.email == email }) {
+        throw DuplicateAccountException()
+      }
     }
   }
 
