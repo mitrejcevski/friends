a82b451add88aa2168943110ad9ec8770f3e76e5
Jov Mit <mitrejcevski@gmail.com>
2021-03-27 00:16:12 +0100

    Removed unused code

--- app/src/main/java/nl/jovmit/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/nl/jovmit/friends/signup/SignUpViewModel.kt
@@ -75,33 +75,5 @@ class SignUpViewModel(
     }
   }
 
-  private fun createUser(
-    email: String,
-    password: String,
-    about: String
-  ): User {
-    checkAccountExists(email)
-    val userId = createUserIdFor(email)
-    val user = User(userId, email, about)
-    saveUser(password, user)
-    return user
-  }
-
-  private fun saveUser(password: String, user: User) {
-    usersForPassword.getOrPut(password, ::mutableListOf).add(user)
-  }
-
-  private fun createUserIdFor(email: String): String {
-    return email.takeWhile { it != '@' } + "Id"
-  }
-
-  private fun checkAccountExists(email: String) {
-    if (usersForPassword.values.flatten().any { it.email == email }) {
-      throw DuplicateAccountException()
-    }
-  }
-
-  private val usersForPassword = mutableMapOf<String, MutableList<User>>()
-
   class DuplicateAccountException : Throwable()
 }
