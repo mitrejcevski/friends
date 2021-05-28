5c5ad20f87f191c660f2542d070bd162cd8be779
Jov Mit <mitrejcevski@gmail.com>
2021-05-28 23:17:12 +0200

    Remove duplication in property names

--- app/src/main/java/nl/jovmit/friends/domain/post/Post.kt
+++ app/src/main/java/nl/jovmit/friends/domain/post/Post.kt
@@ -1,8 +1,8 @@
 package nl.jovmit.friends.domain.post
 
 data class Post(
-  val postId: String,
+  val id: String,
   val userId: String,
-  val postText: String,
+  val text: String,
   val timestamp: Long
 )
