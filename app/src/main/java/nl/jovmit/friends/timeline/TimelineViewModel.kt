698c10218aff1d7ea17c417506b080c6049dff08
Jov Mit <mitrejcevski@gmail.com>
2021-05-29 00:37:29 +0200

    Introduced an InMemporyPostCatalog

--- app/src/main/java/nl/jovmit/friends/timeline/TimelineViewModel.kt
+++ app/src/main/java/nl/jovmit/friends/timeline/TimelineViewModel.kt
@@ -20,18 +20,21 @@ class TimelineViewModel {
       .filter { it.userId == userId }
       .map { it.followedId }
 
-    val postsForUser = postsFor(userIds)
+    val postsForUser = InMemoryPostCatalog().postsFor(userIds)
     mutableTimelineState.value = TimelineState.Posts(postsForUser)
   }
 
-  private fun postsFor(userIds: List<String>): List<Post> {
-    val availablePosts = listOf(
-      Post("postId", "timId", "post text", 1L),
-      Post("post2", "lucyId", "post 2", 2L),
-      Post("post1", "lucyId", "post 1", 1L),
-      Post("post4", "saraId", "post 4", 4L),
-      Post("post3", "saraId", "post 3", 3L)
-    )
-    return availablePosts.filter { userIds.contains(it.userId) }
+  class InMemoryPostCatalog {
+
+    fun postsFor(userIds: List<String>): List<Post> {
+      val availablePosts = listOf(
+        Post("postId", "timId", "post text", 1L),
+        Post("post2", "lucyId", "post 2", 2L),
+        Post("post1", "lucyId", "post 1", 1L),
+        Post("post4", "saraId", "post 4", 4L),
+        Post("post3", "saraId", "post 3", 3L)
+      )
+      return availablePosts.filter { userIds.contains(it.userId) }
+    }
   }
 }
