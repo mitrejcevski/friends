c594c30f2afe1acb4b33fb50bc8580c609ada0e2
Jov Mit <mitrejcevski@gmail.com>
2021-06-26 00:07:02 +0200

    TimelineScreenTest refactoring and cleanup

--- app/src/androidTest/java/nl/jovmit/friends/timeline/TimelineScreenTest.kt
+++ app/src/androidTest/java/nl/jovmit/friends/timeline/TimelineScreenTest.kt
@@ -5,6 +5,7 @@ import nl.jovmit.friends.MainActivity
 import nl.jovmit.friends.domain.post.InMemoryPostCatalog
 import nl.jovmit.friends.domain.post.Post
 import nl.jovmit.friends.domain.post.PostCatalog
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.koin.core.context.loadKoinModules
@@ -32,17 +33,24 @@ class TimelineScreenTest {
     val password = "b0bPaS#2021"
     val post1 = Post("post1", "bobId", "This is Bob's first post", 1L)
     val post2 = Post("post2", "bobId", "Bob's second post is here!", 2L)
-
-    val postsCatalog = InMemoryPostCatalog(listOf(post1, post2))
-    val replaceModule = module {
-      factory<PostCatalog>(override = true) { postsCatalog }
-    }
-    loadKoinModules(replaceModule)
+    replacePostCatalogWith(InMemoryPostCatalog(listOf(post1, post2)))
 
     launchTimelineFor(email, password, timelineTestRule) {
 
     } verify {
       postsAreDisplayed(post1, post2)
     }
+  }
+
+  @After
+  fun tearDown() {
+    replacePostCatalogWith(InMemoryPostCatalog())
+  }
+
+  private fun replacePostCatalogWith(postsCatalog: PostCatalog) {
+    val replaceModule = module {
+      factory(override = true) { postsCatalog }
+    }
+    loadKoinModules(replaceModule)
   }
 }
