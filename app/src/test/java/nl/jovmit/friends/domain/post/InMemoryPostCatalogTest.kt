825d4b0bca54683a6c52ad59ca43be544f645053
Jov Mit <mitrejcevski@gmail.com>
2021-11-02 23:35:38 +0100

    Extracted the data outside of the test

--- app/src/test/java/nl/jovmit/friends/domain/post/InMemoryPostCatalogTest.kt
+++ app/src/test/java/nl/jovmit/friends/domain/post/InMemoryPostCatalogTest.kt
@@ -1,17 +1,18 @@
 package nl.jovmit.friends.domain.post
 
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class InMemoryPostCatalogTest {
 
+  private val tomId = "tomId"
+  private val tomPost = Post("tomPostId", tomId, "Post by Tom", 1L)
+  private val annaPost = Post("annaPostId", "annaId", "Post by Anna", 2L)
+  private val lucyPost = Post("lucyPostId", "lucyId", "Post by Lucy", 3L)
+
   @Test
   fun postFound() = runBlocking {
-    val tomId = "tomId"
-    val tomPost = Post("tomPostId", tomId, "Post by Tom", 1L)
-    val annaPost = Post("annaPostId", "annaId", "Post by Anna", 2L)
-    val lucyPost = Post("lucyPostId", "lucyId", "Post by Lucy", 3L)
     val postCatalog = InMemoryPostCatalog(
       availablePosts = listOf(annaPost, tomPost, lucyPost)
     )
