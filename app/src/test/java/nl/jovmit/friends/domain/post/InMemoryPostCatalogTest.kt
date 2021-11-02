b1e5a0c4f8dc786b973312bfb6d607f488adc7aa
Jov Mit <mitrejcevski@gmail.com>
2021-11-02 23:54:45 +0100

    Extracted a contract test for the PostCatalog

--- app/src/test/java/nl/jovmit/friends/domain/post/InMemoryPostCatalogTest.kt
+++ app/src/test/java/nl/jovmit/friends/domain/post/InMemoryPostCatalogTest.kt
@@ -1,45 +1,8 @@
 package nl.jovmit.friends.domain.post
 
-import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+class InMemoryPostCatalogTest : PostCatalogContract() {
 
-class InMemoryPostCatalogTest {
-
-  private val tomId = "tomId"
-  private val tomPost = Post("tomPostId", tomId, "Post by Tom", 1L)
-  private val annaPost = Post("annaPostId", "annaId", "Post by Anna", 2L)
-  private val lucyPost = Post("lucyPostId", "lucyId", "Post by Lucy", 3L)
-
-  @Test
-  fun postFound() = runBlocking {
-    val postCatalog = postCatalogWith(annaPost, tomPost, lucyPost)
-
-    val result = postCatalog.postsFor(listOf(tomId))
-
-    assertEquals(listOf(tomPost), result)
-  }
-
-  @Test
-  fun postNotFound() = runBlocking {
-    val postCatalog = postCatalogWith(annaPost, lucyPost)
-
-    val result = postCatalog.postsFor(listOf(tomId))
-
-    assertEquals(emptyList<Post>(), result)
-  }
-
-  @Test
-  fun addNewPost() = runBlocking {
-    val postCatalog = postCatalogWith(annaPost, lucyPost)
-    val newlyAddedPost = postCatalog.addPost(tomId, "Tom's new post")
-
-    val result = postCatalog.postsFor(listOf(tomId))
-
-    assertEquals(listOf(newlyAddedPost), result)
-  }
-
-  private fun postCatalogWith(
+  override fun postCatalogWith(
     vararg availablePosts: Post
   ): PostCatalog {
     return InMemoryPostCatalog(
