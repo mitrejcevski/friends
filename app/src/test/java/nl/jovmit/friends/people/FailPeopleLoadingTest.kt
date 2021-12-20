1d825be8dc8b874cb8264c95859d5ae35b6c6533
Jov Mit <mitrejcevski@gmail.com>
2021-12-21 00:58:06 +0100

    Improved the intention of the test and removed obsolete code

--- app/src/test/java/nl/jovmit/friends/people/FailPeopleLoadingTest.kt
+++ app/src/test/java/nl/jovmit/friends/people/FailPeopleLoadingTest.kt
@@ -1,12 +1,11 @@
 package nl.jovmit.friends.people
 
 import nl.jovmit.friends.InstantTaskExecutorExtension
+import nl.jovmit.friends.domain.exceptions.BackendException
 import nl.jovmit.friends.domain.exceptions.ConnectionUnavailableException
-import nl.jovmit.friends.domain.people.InMemoryPeopleCatalog
 import nl.jovmit.friends.domain.people.PeopleCatalog
 import nl.jovmit.friends.domain.people.PeopleRepository
 import nl.jovmit.friends.domain.user.Friend
-import nl.jovmit.friends.domain.user.User
 import nl.jovmit.friends.people.state.PeopleState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -15,22 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(InstantTaskExecutorExtension::class)
 class FailPeopleLoadingTest {
 
-  private val tom = Friend(User("tomId", "", ""), isFollowee = false)
-  private val anna = Friend(User("annaId", "", ""), isFollowee = true)
-  private val sara = Friend(User("saraId", "", ""), isFollowee = false)
-  private val peopleCatalog = InMemoryPeopleCatalog(
-    mapOf(
-      "annaId" to listOf(tom),
-      "lucyId" to listOf(anna, sara, tom),
-      "saraId" to emptyList()
-    )
-  )
-
   @Test
   fun backendError() {
-    val viewModel = PeopleViewModel(PeopleRepository(peopleCatalog))
+    val viewModel = PeopleViewModel(PeopleRepository(UnavailablePeopleCatalog()))
 
-    viewModel.loadPeople("jovId")
+    viewModel.loadPeople(":irrelevant:")
 
     assertEquals(PeopleState.BackendError, viewModel.peopleState.value)
   }
@@ -42,6 +30,13 @@ class FailPeopleLoadingTest {
     viewModel.loadPeople(":irrelevant:")
 
     assertEquals(PeopleState.Offline, viewModel.peopleState.value)
+  }
+
+  private class UnavailablePeopleCatalog : PeopleCatalog {
+
+    override fun loadPeopleFor(userId: String): List<Friend> {
+      throw BackendException()
+    }
   }
 
   private class OfflinePeopleCatalog : PeopleCatalog {
