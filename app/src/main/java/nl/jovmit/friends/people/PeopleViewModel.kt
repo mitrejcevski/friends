8fe9b48ab88afec5dcf99139ed50c9553ea6b84f
Jov Mit <mitrejcevski@gmail.com>
2021-12-21 00:26:34 +0100

    Introduced a class to hold the responsibility of tranlating domain data into ui data

--- app/src/main/java/nl/jovmit/friends/people/PeopleViewModel.kt
+++ app/src/main/java/nl/jovmit/friends/people/PeopleViewModel.kt
@@ -4,8 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import nl.jovmit.friends.domain.exceptions.BackendException
 import nl.jovmit.friends.domain.exceptions.ConnectionUnavailableException
-import nl.jovmit.friends.domain.user.Friend
-import nl.jovmit.friends.domain.user.User
+import nl.jovmit.friends.domain.people.InMemoryPeopleCatalog
 import nl.jovmit.friends.people.state.PeopleState
 
 class PeopleViewModel {
@@ -14,30 +13,20 @@ class PeopleViewModel {
   val peopleState: LiveData<PeopleState> = mutablePeopleState
 
   fun loadPeople(userId: String) {
-    val result = try {
-      val peopleForUserId = InMemoryPeopleCatalog().loadPeopleFor(userId)
-      PeopleState.Loaded(peopleForUserId)
-    } catch (backendException: BackendException) {
-      PeopleState.BackendError
-    } catch (offlineException: ConnectionUnavailableException) {
-      PeopleState.Offline
-    }
+    val result = PeopleRepository().loadPeopleFor(userId)
     mutablePeopleState.value = result
   }
 
-  class InMemoryPeopleCatalog {
-    private val tom = Friend(User("tomId", "", ""), isFollowee = false)
-    private val anna = Friend(User("annaId", "", ""), isFollowee = true)
-    private val sara = Friend(User("saraId", "", ""), isFollowee = false)
-    private val peopleForUserId = mapOf(
-      "annaId" to listOf(tom),
-      "lucyId" to listOf(anna, sara, tom),
-      "saraId" to emptyList()
-    )
-
-    fun loadPeopleFor(userId: String): List<Friend> {
-      if (userId.isBlank()) throw ConnectionUnavailableException()
-      return peopleForUserId[userId] ?: throw BackendException()
+  class PeopleRepository {
+    fun loadPeopleFor(userId: String): PeopleState {
+      return try {
+        val peopleForUserId = InMemoryPeopleCatalog().loadPeopleFor(userId)
+        PeopleState.Loaded(peopleForUserId)
+      } catch (backendException: BackendException) {
+        PeopleState.BackendError
+      } catch (offlineException: ConnectionUnavailableException) {
+        PeopleState.Offline
+      }
     }
   }
 }
