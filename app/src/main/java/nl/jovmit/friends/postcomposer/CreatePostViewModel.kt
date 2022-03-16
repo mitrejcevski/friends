525932520cf815713c7d54916b34ab1e08ff8994
Jov Mit <mitrejcevski@gmail.com>
2022-03-16 22:11:11 +0100

    Introduce saved state handle

--- app/src/main/java/nl/jovmit/friends/postcomposer/CreatePostViewModel.kt
+++ app/src/main/java/nl/jovmit/friends/postcomposer/CreatePostViewModel.kt
@@ -1,13 +1,11 @@
 package nl.jovmit.friends.postcomposer
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import nl.jovmit.friends.app.CoroutineDispatchers
 import nl.jovmit.friends.domain.post.PostRepository
+import nl.jovmit.friends.postcomposer.state.CreateNewPostScreenState
 import nl.jovmit.friends.postcomposer.state.CreatePostState
 
 class CreatePostViewModel(
@@ -18,6 +16,10 @@ class CreatePostViewModel(
   private val mutablePostState = MutableLiveData<CreatePostState>()
   val postState: LiveData<CreatePostState> = mutablePostState
 
+  private val savedStateHandle = SavedStateHandle()
+  val postScreenState: LiveData<CreateNewPostScreenState> =
+    savedStateHandle.getLiveData(SCREEN_STATE_KEY)
+
   fun createPost(postText: String) {
     viewModelScope.launch {
       mutablePostState.value = CreatePostState.Loading
@@ -25,5 +27,9 @@ class CreatePostViewModel(
         postRepository.createNewPost(postText)
       }
     }
+  }
+
+  private companion object {
+    private const val SCREEN_STATE_KEY = "createPostScreenState"
   }
 }
