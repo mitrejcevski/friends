4e705a8dabba3a7cddeb40e06d9a057d74866bb1
Ivan Mamula <mamula82@gmail.com>
2025-11-10 21:46:27 +0100

    Updated kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.parcelable) apply false
+  alias(libs.plugins.compose.plugin) apply false
 }
